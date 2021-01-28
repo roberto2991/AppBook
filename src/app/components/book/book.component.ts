584f0700fdff12bc28162ba30e18b30c0fe6aaf9
roberto2991 <robertomasellis@gmail.com>
2021-01-28 20:23:37 +0100

    creazione della struttura base

---
+++ src/app/components/book/book.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  template: `
+    <p>
+      book works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class BookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
