232ced32f0ea20c371d76471f7983d7df15a7552
Bob <robertomasellis@gmail.com>
2021-03-23 16:32:55 +0100

    edit record and reset form

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -2,7 +2,7 @@ import { Book } from './../../model/book';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { error } from '@angular/compiler/src/util';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 
 
 const dbUrl = "http://localhost:3000/books";
@@ -18,8 +18,25 @@ export class BookComponent implements OnInit {
 
   books: Book[];
   err: any;
+  active: Book;
 
   constructor(private http: HttpClient) { }
+
+  save(f: NgForm) {
+    if (this.active) {
+      this.edit(f)
+    } else {
+      this.AddBook(f);
+    }
+  }
+
+  edit(f: NgForm) {
+    this.http.patch<Book>(`${dbUrl}/${this.active.id}`, f.value).subscribe(data => {
+      const idx = this.books.findIndex(b => b.id === this.active.id);
+      this.books[idx] = data;
+      console.log('edit element');
+    });
+  }
 
   AddBook(form: NgForm) {
     console.log(form);
@@ -37,19 +54,28 @@ export class BookComponent implements OnInit {
     );
   }
 
-  delete(book: Book) {
+  delete(event: Event, book: Book) {
+    
+    event.stopPropagation();
     this.http.delete<Book[]>(`${dbUrl}/${book.id}`).subscribe(next => {
       const idx = this.books.findIndex(b => b.id === book.id);
       this.books.splice(idx, 1);
     },
       (error) => this.err = error
     );
+  }
 
+  reset(f: NgForm) {
+    this.active = null;
+    f.reset();
+  }
+
+  setActive(book: Book) {
+    this.active = book;
   }
 
   ngOnInit(): void {
     this.getAll();
-
   }
 
 }
